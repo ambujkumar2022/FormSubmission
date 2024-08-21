2f97d272497dd1ccc338730946f97e3f3c877ee0
ambujkumar2022 <108212872+ambujkumar2022@users.noreply.github.com>
2024-08-21 14:49:36 +0200

    Add files via upload
    bin, Migrations, Model, obj files are committed

---
+++ Model/AuthDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormSubmission.Model
+{
+	public class AuthDbContext : IdentityDbContext<IdentityUser>
+	{
+		public AuthDbContext(DbContextOptions<AuthDbContext> options): base(options)
+		{
+
+		}
+	}
+
+	public class AuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+	{		public AuthDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<AuthDbContext>();
+			optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=AspNetAuth;Trusted_Connection=True;Integrated Security=True");
+			//optionsBuilder.UseSqlServer("AuthConnString");
+			return new AuthDbContext(optionsBuilder.Options);
+		}
+	}
+}

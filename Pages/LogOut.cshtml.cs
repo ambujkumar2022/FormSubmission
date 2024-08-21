e50cef1896ce73da819c5d1940c1bbdc2d29f697
ambujkumar2022 <108212872+ambujkumar2022@users.noreply.github.com>
2024-08-21 14:52:08 +0200

    Add files via upload
    Pages, Properties, ViewModels, wwwroot files are committed

---
+++ Pages/LogOut.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FormSubmission.Pages
+{
+    public class LogOutModel : PageModel
+    {
+		private readonly SignInManager<IdentityUser> signInManager;
+
+		public LogOutModel(SignInManager<IdentityUser> signInManager)
+        {
+			this.signInManager = signInManager;
+		}
+        public void OnGet()
+        {
+        }
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToPage("LogIn");
+        }
+        public IActionResult OnPostDontLogutAsync()
+        {
+			return RedirectToPage("LogIn");
+		}
+    }
+}
---
+++ ViewModels/Register.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.ViewModels
+{
+	public class Register
+	{
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		[Compare(nameof(Password), ErrorMessage = "Password and Configuration password did not match")]
+		public string ConfirmPassword { get; set;}
+	}
+}
